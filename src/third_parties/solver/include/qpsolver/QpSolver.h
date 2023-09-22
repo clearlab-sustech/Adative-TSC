6117bb21bb0bd42c0e04ae7954f54b9a51df6ba7
MoreInfo <1358458373@qq.com>
2023-09-22 18:46:57 +0800

    update

--- src/third_parties/solver/include/qpsolver/QpSolver.h
+++ src/third_parties/solver/include/qpsolver/QpSolver.h
@@ -2,18 +2,129 @@
 
 #include <core/types.h>
 
+#include <blasfeo_d_aux_ext_dep.h>
+#include <hpipm_d_dense_qp.h>
+#include <hpipm_d_dense_qp_dim.h>
+#include <hpipm_d_dense_qp_ipm.h>
+#include <hpipm_d_dense_qp_sol.h>
+#include <hpipm_d_dense_qp_utils.h>
+#include <hpipm_timing.h>
+
 namespace clear {
+
+typedef d_dense_qp_dim DimsSpec;
+
+typedef d_dense_qp_sol QpSolution;
+
+typedef d_dense_qp_ipm_arg SolverConfig;
+
 class QpSolver {
 
 public:
-  QpSolver(/* args */);
+  struct QpSolverSettings {
+  public:
+    ///
+    /// @brief Solver mode. Default is hpipm_mode::SPEED.
+    ///
+    hpipm_mode mode = hpipm_mode::SPEED;
+
+    ///
+    /// @brief Maximum number of iterations. Must be nen-negative. Default
+    /// is 15.
+    ///
+    int iter_max = 15;
+
+    ///
+    /// @brief Minimum step size. Must be positive and less than 1.0. Default
+    /// is 1.0e-08.
+    ///
+    double alpha_min = 1.0e-08;
+
+    ///
+    /// @brief Initial barrier parameter. Must be positive. Default is 1.0e+02.
+    ///
+    double mu0 = 1.0e+02;
+
+    ///
+    /// @brief Convergence criteria. Must be positive. Default is 1.0e-08.
+    ///
+    double tol_stat = 1.0e-08;
+
+    ///
+    /// @brief Convergence criteria. Must be positive. Default is 1.0e-08.
+    ///
+    double tol_eq = 1.0e-08;
+
+    ///
+    /// @brief Convergence criteria. Must be positive. Default is 1.0e-08.
+    ///
+    double tol_ineq = 1.0e-08;
+
+    ///
+    /// @brief Convergence criteria. Must be positive. Default is 1.0e-08.
+    ///
+    double tol_comp = 1.0e-08; // convergence criteria
+
+    ///
+    /// @brief Regularization term. Must be non-negative. Default is 1.0e-12.
+    ///
+    double reg_prim = 1.0e-12;
+
+    ///
+    /// @brief Warm start flag (0: disable, 1: enable). Default is 0.
+    ///
+    int warm_start = 0;
+
+    ///
+    /// @brief Prediction-correction flag (0: disable, 1: enable). Default is 1.
+    ///
+    int pred_corr = 1;
+
+    ///
+    /// @brief Square-root Riccati flag (0: disable, 1: enable). Default is 1.
+    ///
+    int ric_alg = 1;
+
+    ///
+    /// @brief Use different step for primal and dual variables (0: disable, 1:
+    /// enable). Default is 0.
+    ///
+    int split_step = 0;
+
+    ///
+    /// @brief Check the settings. If something is wrong, throws an exception.
+    ///
+    void checkSettings() const;
+  };
+
+public:
+  QpSolver(const DimsSpec &dims);
+
   ~QpSolver();
 
-  matrix_t H, Aeq, C;
-  vector_t g, beq, d;
+  void update(Eigen::Ref<matrix_t> H, Eigen::Ref<vector_t> g,
+              Eigen::Ref<matrix_t> A, Eigen::Ref<vector_t> b,
+              Eigen::Ref<matrix_t> C, Eigen::Ref<vector_t> lb,
+              Eigen::Ref<vector_t> ub);
+
+  vector_t solve();
 
 private:
-  /* data */
+  DimsSpec dims_;
+  matrix_t H, A, C;
+  vector_t g, b, lb, ub, sol;
+
+  hpipm_size_t dim_size;
+  struct d_dense_qp qp;
+  void *dim_mem;
+  void *qp_mem;
+
+  hpipm_size_t qp_sol_size;
+  void *qp_sol_mem;
+  QpSolution qp_sol;
+
+  hpipm_size_t ipm_arg_size;
+  SolverConfig arg;
 };
 
 } // namespace clear
--- src/third_parties/solver/src/QpSolver.cc
+++ src/third_parties/solver/src/QpSolver.cc
@@ -1,8 +1,116 @@
 #include "qpsolver/QpSolver.h"
 
 namespace clear {
-QpSolver::QpSolver(/* args */) {}
 
-QpSolver::~QpSolver() {}
+void QpSolver::QpSolverSettings::checkSettings() const {
+  if (iter_max < 0) {
+    throw std::runtime_error(
+        "OcpQpIpmSolverSettings.iter_max must be non-negative");
+  }
+  if (alpha_min <= 0) {
+    throw std::runtime_error(
+        "OcpQpIpmSolverSettings.alpha_min must be positive");
+  }
+  if (alpha_min > 1.0) {
+    throw std::runtime_error(
+        "OcpQpIpmSolverSettings.alpha_min must be less than 1.0");
+  }
+  if (mu0 <= 0.0) {
+    throw std::runtime_error("OcpQpIpmSolverSettings.mu0 must be positive");
+  }
+  if (tol_stat <= 0.0) {
+    throw std::runtime_error(
+        "OcpQpIpmSolverSettings.tol_stat must be positive");
+  }
+  if (tol_eq <= 0.0) {
+    throw std::runtime_error("OcpQpIpmSolverSettings.tol_eq must be positive");
+  }
+  if (tol_ineq <= 0.0) {
+    throw std::runtime_error(
+        "OcpQpIpmSolverSettings.tol_ineq must be positive");
+  }
+  if (tol_comp <= 0.0) {
+    throw std::runtime_error(
+        "OcpQpIpmSolverSettings.tol_comp must be positive");
+  }
+  if (reg_prim < 0.0) {
+    throw std::runtime_error(
+        "OcpQpIpmSolverSettings.reg_prim must be non-negative");
+  }
+  if (reg_prim < 0.0) {
+    throw std::runtime_error(
+        "OcpQpIpmSolverSettings.reg_prim must be non-negative");
+  }
+}
+
+QpSolver::QpSolver(const DimsSpec &dims) : dims_(dims) {
+
+  dim_size = d_dense_qp_dim_memsize();
+  dim_mem = malloc(dim_size);
+  d_dense_qp_dim_create(&dims_, dim_mem);
+
+  hpipm_size_t qp_size = d_dense_qp_memsize(&dims_);
+  void *qp_mem = malloc(qp_size);
+  d_dense_qp_create(&dims_, &qp, qp_mem);
+
+  qp_sol_size = d_dense_qp_sol_memsize(&dims_);
+  qp_sol_mem = malloc(qp_sol_size);
+
+  ipm_arg_size = d_dense_qp_ipm_arg_memsize(&dims_);
+  void *ipm_arg_mem = malloc(ipm_arg_size);
+
+  d_dense_qp_ipm_arg_create(&dims_, &arg, ipm_arg_mem);
+
+  d_dense_qp_ipm_arg_set_default(hpipm_mode::SPEED, &arg);
+
+  d_dense_qp_ipm_arg_set_mu0(&mu0, &arg);
+  d_dense_qp_ipm_arg_set_iter_max(&iter_max, &arg);
+  d_dense_qp_ipm_arg_set_alpha_min(&alpha_min, &arg);
+  d_dense_qp_ipm_arg_set_mu0(&mu0, &arg);
+  d_dense_qp_ipm_arg_set_tol_stat(&tol_stat, &arg);
+  d_dense_qp_ipm_arg_set_tol_eq(&tol_eq, &arg);
+  d_dense_qp_ipm_arg_set_tol_ineq(&tol_ineq, &arg);
+  d_dense_qp_ipm_arg_set_tol_comp(&tol_comp, &arg);
+  d_dense_qp_ipm_arg_set_reg_prim(&reg_prim, &arg);
+  d_dense_qp_ipm_arg_set_reg_dual(&reg_dual, &arg);
+  d_dense_qp_ipm_arg_set_warm_start(&warm_start, &arg);
+  d_dense_qp_ipm_arg_set_pred_corr(&pred_corr, &arg);
+  d_dense_qp_ipm_arg_set_split_step(&split_step, &arg);
+}
+
+QpSolver::~QpSolver() {
+  free(dim_mem);
+  free(qp_mem);
+  free(qp_sol_mem);
+  free(ipm_arg_mem);
+  free(ipm_mem);
+
+  free(v);
+}
+
+void QpSolver::update(Eigen::Ref<matrix_t> H, Eigen::Ref<vector_t> g,
+                      Eigen::Ref<matrix_t> A, Eigen::Ref<vector_t> b,
+                      Eigen::Ref<matrix_t> C, Eigen::Ref<vector_t> lb,
+                      Eigen::Ref<vector_t> ub) {
+  this->H = H;
+  this->g = g;
+  this->A = A;
+  this->b = b;
+  this->C = C;
+  this->lb = lb;
+  this->ub = ub;
+}
+
+vector_t QpSolver::solve() {
+  d_dense_qp_set_all(H.data(), g.data(), A.data(), b.data(), NULL, NULL, NULL,
+                     C.data(), lb.data(), ub.data(), NULL, NULL, NULL, NULL,
+                     NULL, NULL, NULL, &qp);
+
+  d_dense_qp_sol_create(&dims_, &qp_sol, qp_sol_mem);
+
+  sol.setZero(dims_.nv);
+
+  return sol;
+}
 
 } // namespace clear
